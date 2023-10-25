b12d17c5832d362e28eb064ca29848df710c06a5
XADEMX72 <77357957+XADEMX72@users.noreply.github.com>
2023-10-25 22:24:51 +0400

    documenting functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
-def ytd(yt):
+# Function to download audio from a YouTube video using the pytube library.
+# It takes a pytube YouTube object as input.
+def download_audio(yt):
+    # Get the best available audio-only stream in mp4 format.
     stream = yt.streams.get_audio_only('mp4')
+    
+    # Download the audio stream to the 'audio vids' directory.
     stream.download(output_path='audio vids')
-def vid(mp4):
+
+# Function to download the highest resolution video from a YouTube video using the pytube library.
+# It takes a pytube YouTube object as input.
+def download_video(mp4):
+    # Get the highest resolution video stream that is not audio-only.
     stream = mp4.streams.filter(only_video=False).get_highest_resolution()
+    
+    # Download the video stream to the 'vids' directory.
     stream.download(output_path='vids')
