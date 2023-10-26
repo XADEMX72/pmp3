ac6917cd841b68d85a8c2a02e17b862fe82f45e5
xa72 <xademx72@gmail.com>
2023-10-26 22:10:38 +0400

    Modified functions.py to enable user-specified file download names, added pycache to. gitignore, and introduced a loop with a user exit option in main.py.

--- functions.py
+++ functions.py
@@ -3,15 +3,23 @@
 def download_audio(yt):
     # Get the best available audio-only stream in mp4 format.
     stream = yt.streams.get_audio_only('mp4')
-    
+
+    file_name = input("What would you like to name the file: ")
+
+    file_name_prefix = file_name + ".mp4"
+
     # Download the audio stream to the 'audio vids' directory.
-    stream.download(output_path='audio vids')
+    stream.download(output_path='audio files', filename=file_name_prefix)
 
 # Function to download the highest resolution video from a YouTube video using the pytube library.
 # It takes a pytube YouTube object as input.
 def download_video(mp4):
     # Get the highest resolution video stream that is not audio-only.
     stream = mp4.streams.filter(only_video=False).get_highest_resolution()
-    
+
+    file_name = input("What would you like to name the file: ")
+
+    file_name_prefix = file_name + ".mp4"
+
     # Download the video stream to the 'vids' directory.
-    stream.download(output_path='vids')
+    stream.download(output_path='videos files', filename=file_name_prefix)
--- main.py
+++ main.py
@@ -1,40 +1,60 @@
+# Import the necessary module 'YouTube' from the 'pytube' library and 'functions' module
 from pytube import YouTube
 from functions import *
 
+# Display a welcome message
 print("Welcome to pmp3 | By XA72")
 
+# Create an infinite loop that keeps running until the user chooses to exit
 while True:
-    user_i = input("| 1 | For Audio Only | 2 | For Video: ")
+    # Prompt the user to choose an option
+    user_i = input("| 1 | For Audio Only | 2 | For Video | 3 | To Exit: ")
+    # Check if the user wants to exit
+    if user_i == "3":
+        break  # Exit the loop if the user chooses option 3
 
-    if user_i == "1":
+    # Check if the user wants to download audio
+    elif user_i == "1":
+        # Create an infinite loop to get a valid YouTube video link
         while True:
             url = input("The link of the YouTube video: ")
             if url:
-                break
+                break  # Exit the loop if a non-empty URL is provided
             else:
                 print("Invalid Input")
+
+        # Check if the provided URL is a valid YouTube URL
         if "https://www.youtube.com/" in url:
             # Initialize a YouTube object with the provided URL
             youtube_audio = YouTube(url)
-            # Call the download_audio function to download the audio
+            # Call the 'download_audio' function to download the audio
             download_audio(youtube_audio)
         else:
             print("The input does not contain 'https://www.youtube.com/'.")
+
         break  # Exit the loop after successful input
+
+    # Check if the user wants to download video
     elif user_i == "2":
+        # Create an infinite loop to get a valid YouTube video link
         while True:
             url = input("The link of the YouTube video: ")
             if url:
-                break
+                break  # Exit the loop if a non-empty URL is provided
             else:
                 print("Invalid Input")
+
+        # Check if the provided URL is a valid YouTube URL
         if "https://www.youtube.com/" in url:
             # Initialize a YouTube object with the provided URL
             youtube_video = YouTube(url)
-            # Call the download_video function to download the video
+            # Call the 'download_video' function to download the video
             download_video(youtube_video)
         else:
             print("The input does not contain 'https://www.youtube.com/'.")
+
         break  # Exit the loop after successful input
+
+    # Handle invalid user input
     else:
         print("Invalid input. Please enter 1 or 2.")
